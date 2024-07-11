1698167eb1ce2cf5a6ede2a96cae5fdaef6d0886
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-11 09:33:47 +0300

    defining the models - complete

--- src/main/java/com/tools/demo/api/Category.java
+++ src/main/java/com/tools/demo/api/Category.java
@@ -1,4 +1,8 @@
 package com.tools.demo.api;
 
+// DEFINING AN ENUM TO BE USED IN THE CATEGORY ATTRIBUTE IN THE MODEL CLASS
 public enum Category {
+    Farm,
+    Work,
+    Home
 }
--- src/main/java/com/tools/demo/api/ToolModel.java
+++ src/main/java/com/tools/demo/api/ToolModel.java
@@ -1,6 +1,10 @@
 package com.tools.demo.api;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +14,23 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name = "tool")
 public class ToolModel {
 
+    @Id
     private UUID id;
     private String name;
     private Integer quantity;
-    private String category;
+    // I utilized the enum class that I created to define that the category will only allow Home, Work, and Farm
+    private Category category;
+
+    @PrePersist
+    public void generateId(){
+        if(this.id == null){
+            this.id = UUID.randomUUID();
+        }
+    }
+
+
 }
