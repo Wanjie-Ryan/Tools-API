041524457e4f1ea7693be4db4e3b2e8fddbb0280
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-11 09:24:49 +0300

    defining the models

--- src/main/java/com/tools/demo/api/ToolModel.java
+++ src/main/java/com/tools/demo/api/ToolModel.java
@@ -1,4 +1,19 @@
 package com.tools.demo.api;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ToolModel {
+
+    private UUID id;
+    private String name;
+    private Integer quantity;
+    private String category;
 }
