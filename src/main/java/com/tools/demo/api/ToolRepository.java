8cfb8ac5abdec742484b436bb32e6833061ee9d0
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-11 09:48:55 +0300

    defining the repository - pass in the model you want to use and the primary key

--- src/main/java/com/tools/demo/api/ToolRepository.java
+++ src/main/java/com/tools/demo/api/ToolRepository.java
@@ -1,4 +1,12 @@
 package com.tools.demo.api;
 
-public interface ToolRepository {
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+// you specify the type of entity and the type of its primary key that the repository will handle
+@Repository
+public interface ToolRepository extends JpaRepository<ToolModel, UUID> {
 }
